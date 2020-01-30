eed03db8f8f2341cee32028d6c41bfee178f8f3b
eir73 <50501888+eir73@users.noreply.github.com>
2020-01-30 14:37:35 +0200

    Добавил забытый массив студентов в схему школы

--- SchoolModel.js
+++ SchoolModel.js
@@ -1,27 +1,39 @@
 
 const TeacherSchema = require('./TeacherModel.js').schema
 const LessonSchema = require('./LessonModel.js').schema
+const StudentSchema = require('./StudentModel.js').schema
 
 const mongoose = require('mongoose')
 
 const School = new mongoose.Schema({
-    name: {
+    number: {
         type: Number,
         required: true
     },
     groups: {
         type: [String],
+        default: [],
         maxlength: 100,
-        required: true
     },
     teachers: {
         type: [TeacherSchema],
+        default: [],
         maxlength: 50,
-        required: true
     },
     lessons: {
         type: [LessonSchema],
-        required: true,
+        default: [],
         maxlength: 50
+    },
+    students: {
+        type: [StudentSchema],
+        default: [],
+        maxlength: 2500
     }
 })
+
+
+module.exports = {
+    model: mongoose.model('school', School),
+    schema: School
+}
